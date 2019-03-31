53a784816e0ec6852099008cb29827ae9a542e9e
Simeon McGraw <simeon@smcgraw.com>
2019-03-30 22:06:51 -0400

    Added team elements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import { Button } from '@material-ui/core';
+import { 
+	AppBar, 
+	Toolbar, 
+	Typography, 
+	TextField, 
+	Table, TableBody, TableCell, TableRow, 
+	IconButton, Button,
+	Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle,
+} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
 
 const styles = (theme) => ({
 	root: {
@@ -16,22 +20,40 @@ const styles = (theme) => ({
 	grow: {
 		flexGrow: 1,
 	},
-	menuButton: {
-		marginLeft: -12,
-		marginRight: 20,
+	body: {
+		padding: 20,
+		maxWidth: 700,
+		margin: "0 auto",
+		right: 0, left: 0,
+	},
+	table: {
+		maxWidth: 700,
+	},
+	removeButton: {
+		float: "right",
 	},
 	textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-    width: 200,
-  },
+		marginLeft: theme.spacing.unit,
+		marginRight: theme.spacing.unit,
+		width: "100%",
+	},
+	addButton: {
+		// float: "right",
+	},
+	plotButton: {
+		margin: 5,
+	},
 });
 
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			items: ["", ""],
+			dialogTitle: "",
+			dialogText: "",
+			dialogOpened: false,
+			teamSize: 2,
+			items: [{name: "", idea: ""}, {name: "", idea: ""}],
 			output: "",
 		};
 	}
@@ -46,34 +68,71 @@ class App extends React.Component {
 						</Typography>
 					</Toolbar>
 				</AppBar>
-				<List>
-				{
-					this.state.items.map((item, index) => {
-						return (
-							<ListItem key={index}>
-								<TextField
-									label="item"
-
-									className={classes.textField}
-									value={item}
-									onChange={this.handleChange(index)}
-									margin="normal"
-									/>
-								<Button variant="contained" color="secondary" onClick={this.removeInput(index)}>Remove</Button>
-							</ListItem>
-						);
-					})
-				}
-				</List>
-				<Button variant="contained" onClick={this.addInput}>
-					Add
-				</Button>
-				<Button variant="contained" color="primary" onClick={this.getOutput}>
-					Plot
-				</Button>
+				<div className={classes.body}>
+					<TextField
+						label="Team Size"
+						defaultValue={this.state.teamSize}
+						className={classes.teamSizeInput}
+						onChange={this.handleSizeChange}
+						margin="normal"
+						/>
+					<Table className={classes.table}>
+						<TableBody>
+							{this.state.items.map((item, index) => {
+								return (
+									<TableRow key={index}>
+										<TableCell>
+											<TextField
+												label="Name"
+												className={classes.textField}
+												onChange={this.handleChange(index)}
+												margin="normal"
+												/>
+										</TableCell>
+										<TableCell>
+											<TextField
+												label="Idea"
+												className={classes.textField}
+												onChange={this.handleChange(index)}
+												margin="normal"
+												/>
+										</TableCell>
+										<TableCell>
+											<IconButton className={classes.removeButton} variant="outlined" onClick={this.removeInput(index)}>
+												<DeleteIcon />
+											</IconButton>
+										</TableCell>
+									</TableRow>
+								);
+							})}
+						</TableBody>
+					</Table>
+					<Button className={classes.addButton} onClick={this.addInput}> <AddIcon /> Add Person</Button>
+					<br />
+					<br />
+					<Button className={classes.plotButton} variant="contained" color="primary" onClick={this.getOutput}>Plot</Button>
 				<div>
 					<img src={this.state.output} alt="output" />
 				</div>
+				</div>
+				<Dialog
+					open={this.state.dialogOpened}
+					onClose={this.closeDialog}
+					aria-labelledby="alert-dialog-title"
+					aria-describedby="alert-dialog-description" >
+				{this.state.dialogTitle.length > 0 &&
+					<DialogTitle id="alert-dialog-title">{this.state.dialogTitle}</DialogTitle>}
+				<DialogContent>
+					<DialogContentText id="alert-dialog-description">
+						{this.state.dialogText}
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button varient="contained" onClick={this.closeDialog} autoFocus>
+					Close
+					</Button>
+				</DialogActions>
+				</Dialog>
 			</div>
 		);
 	}
@@ -87,13 +146,36 @@ class App extends React.Component {
 		items[index] = event.target.value;
 		this.setState({items});
 	}
+	handleSizeChange = event => {
+		const stringValue = event.target.value;
+		const value = parseInt(stringValue);
+		this.setState({teamSize: isNaN(value) ? undefined : value });
+	}
 	addInput = () => {
 		const items = this.state.items;
-		items.push("");
+		items.push({name: "", idea: ""});
 		this.setState({items});
 	}
 	getOutput = () => {
+		if (typeof this.state.teamSize !== "number") {
+			this.openDialog("Team size not valid.");
+			return;
+		}
+		if (typeof this.state.teamSize < 2) {
+			this.openDialog("Team size must be 2 or more!");
+			return;
+		}
+		if (this.state.items.length % this.state.teamSize !== 0) {
+			this.openDialog("You must have enough people to divide into even teams!");
+			return;
+		}
 		this.setState({ output: "https://chartio.com/images/tutorials/scatter-plot/Scatter-Plot-Weight-and-Height-Scatter-Plot-Trendline.png" })
+	}
+	openDialog = (text, title="") => {
+		this.setState({ dialogOpened: true, dialogText: text, dialogTitle: title });
+	}
+	closeDialog = () => {
+		this.setState({ dialogOpened: false });
 	}
 }
 
